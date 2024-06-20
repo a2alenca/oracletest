5664c05c306ac4989af9ca6f8315c174d1f16037
a2alenca <77257840+a2alenca@users.noreply.github.com>
2024-06-19 23:04:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ let Palabra = "vibora";
 
 console.log(PalabraElegida);
 
-if (Palabra == PalabraElegida) { alert("Sipos vibora es correcto "); location.href = "https://www.youtube.com/shorts/XDjL8mIY6SE"; } else { alert("Incorrecto upsie ")};  
+if (Palabra == PalabraElegida) { alert("Sipos vibora es correcto "); location.href = "https://www.youtube.com/shorts/XDjL8mIY6SE"; } else { alert("Incorrecto upsie ");location.reload(); };  
