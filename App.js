df43b95b91d39ab8c098261032018c9b1bc6533b
a2alenca <77257840+a2alenca@users.noreply.github.com>
2024-06-19 22:29:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-  
+alert('"Bienvenida Rata".');
+alert('"Debes adivinar la palabra secreta".');
+let PalabraElegida = prompt("Que palabra eliges?");
+let Palabra = "raton";
+
+console.log(PalabraElegida);
+
+if (Palabra == PalabraElegida) { alert("Sipos raton, correcto"); } else { alert("Incorrecto upsie ")};  
